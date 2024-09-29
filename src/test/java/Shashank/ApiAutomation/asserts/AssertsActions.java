99cc184b13f2238370f73f7858baf28d9ab341ea
shashanks-06 <surjekarshashank@gmail.com>
2024-09-29 22:19:16 +0530

    assertions

--- src/test/java/Shashank/ApiAutomation/asserts/AssertsActions.java
+++ src/test/java/Shashank/ApiAutomation/asserts/AssertsActions.java
@@ -1,5 +1,19 @@
 package Shashank.ApiAutomation.asserts;
 
+import io.restassured.response.Response;
+import org.testng.Assert;
+
 public class AssertsActions {
     // Common Assertions - Which can be reused.
+
+    public void verifyResponseBody(String actual, String expected, String description){
+        Assert.assertEquals(actual, expected, description);
+    }
+    public void verifyResponseBody(int actual, int expected, String description){
+        Assert.assertEquals(actual, expected, description);
+    }
+
+    public void verifyStatusCode(Response response, Integer expected){
+        Assert.assertEquals(response.getStatusCode(), expected);
+    }
 }
